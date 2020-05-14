6ff9182d41a1ff2dcf57f29bfecb75a964a7e387
zodiac12k <zodiac12k@gmail.com>
2020-05-14 10:23:52 +0900

    Cleaning

--- kubernetes-dashboard/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-helm install stable/kubernetes-dashboard --name kubernetes-dashboard --namespace=kube-system -f https://raw.githubusercontent.com/cloudz-cp/zcp-base/master/kubernetes-dashboard/values-eks.yaml \
---set ingress.annotations."kubernetes\.io/ingress\.class"=nginx \
---set ingress.hosts[0]="fsk-dashboard.cloudzcp.io" \
---set ingress.tls[0].hosts[0]="fsk-dashboard.cloudzcp.io"
